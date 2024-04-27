be0c22b7c230de873ecc916b58611a781e8057a1
Jayasvi <80738746+snehal29j@users.noreply.github.com>
2024-04-27 14:42:21 +0000

    first

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 Language: JAVA
 
 Networking: HTTP, TCP/IP, Sockets
+
+1. Client-Side Program: A client can communicate with a server using this code. This involves 
+    - Establish a Socket Connection
+    - Communication
+
+2. Server-Side Program: 
+    a) Server class: 
+        Establishing the Connection: Server socket object is initialized and inside a while loop a socket object continuously accepts an incoming connection.
+        Obtaining the Streams: The inputstream object and outputstream object is extracted from the current requests’ socket object.
+        Creating a handler object: After obtaining the streams and port number, a new clientHandler object is created with these parameters.
+        Invoking the start() method: The start() method is invoked on this newly created thread object.
+
+    b) ClientHandler class: 
+        An object of this class acts as a Runnable target for a new thread.
+        First, this class implements Runnable interface so that it can be passed as a Runnable target while creating a new Thread.
+        Secondly, the constructor of this class takes a parameter, which can uniquely identify any incoming request, i.e. a Socket.
+        Inside the run() method of this class, it reads the client’s message and replies.
---
+++ Server.java
@@ -0,0 +1,92 @@
+import java.io.*; 
+import java.net.*; 
+ 
+class Server { 
+	public static void main(String[] args) 
+	{ 
+		ServerSocket server = null; 
+
+		try { 
+
+			server = new ServerSocket(1234); 
+			server.setReuseAddress(true); 
+
+			while (true) { 
+
+				Socket client = server.accept(); 
+
+				System.out.println("New client connected"
+								+ client.getInetAddress() 
+										.getHostAddress()); 
+
+				ClientHandler clientSock 
+					= new ClientHandler(client); 
+
+				new Thread(clientSock).start(); 
+			} 
+		} 
+		catch (IOException e) { 
+			e.printStackTrace(); 
+		} 
+		finally { 
+			if (server != null) { 
+				try { 
+					server.close(); 
+				} 
+				catch (IOException e) { 
+					e.printStackTrace(); 
+				} 
+			} 
+		} 
+	} 
+
+	private static class ClientHandler implements Runnable { 
+		private final Socket clientSocket; 
+
+		public ClientHandler(Socket socket) 
+		{ 
+			this.clientSocket = socket; 
+		} 
+
+		public void run() 
+		{ 
+			PrintWriter out = null; 
+			BufferedReader in = null; 
+			try { 
+
+				out = new PrintWriter( 
+					clientSocket.getOutputStream(), true); 
+
+				in = new BufferedReader( 
+					new InputStreamReader( 
+						clientSocket.getInputStream())); 
+
+				String line; 
+				while ((line = in.readLine()) != null) { 
+ 
+					System.out.printf( 
+						" Sent from the client: %s\n", 
+						line); 
+					out.println(line); 
+				} 
+			} 
+			catch (IOException e) { 
+				e.printStackTrace(); 
+			} 
+			finally { 
+				try { 
+					if (out != null) { 
+						out.close(); 
+					} 
+					if (in != null) { 
+						in.close(); 
+						clientSocket.close(); 
+					} 
+				} 
+				catch (IOException e) { 
+					e.printStackTrace(); 
+				} 
+			} 
+		} 
+	} 
+}
